5c070cd1ee5dcb84b0ec08e2fd4d49595e3ab63c
Journey <timtag1190@gmail.com>
2025-02-15 13:40:05 -0600

    Remove custom link provider definitions and related code
    I have plans on adding them back at some point in the near future, but I need to figure out a better method.
    
    Add extra parsing from https://github.com/BeeeQueue/arm-server api for anime.

--- libs/wikidata/index.js
+++ libs/wikidata/index.js
@@ -5,10 +5,11 @@
 // @name         @journeyover/wikidata
 // @description  Wikidata for my userscripts
 // @license      MIT
-// @version      1.0.0
+// @version      1.1.0
 // @homepageURL  https://github.com/StylusThemes/Userscripts
 // ==/UserLibrary==
 // @connect      query.wikidata.org
+// @connect      arm.haglund.dev
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
@@ -37,16 +38,16 @@ this.Wikidata = class {
             return "P1258";
           case "Metacritic":
             return "P1712";
+          case "Letterboxd":
+            return "P6127";
+          case "TVmaze":
+            return "P8600";
           case "MyAnimeList":
             return "P4086";
           case "AniDB":
             return "P5646";
           case "AniList":
             return "P8729";
-          case "Letterboxd":
-            return "P6127";
-          case "TVmaze":
-            return "P8600";
           default:
             throw new Error("An ID source is required");
         }
@@ -79,60 +80,172 @@ this.Wikidata = class {
             return { value: `https://www.rottentomatoes.com/${t}` };
           case "Metacritic":
             return { value: `https://www.metacritic.com/${t}` };
+          case "Letterboxd":
+            return { value: `https://letterboxd.com/film/${t}` };
+          case "TVmaze":
+            return { value: `https://tvmaze.com/shows/${t}` };
           case "MyAnimeList":
             return { value: `https://myanimelist.net/anime/${t}` };
           case "AniDB":
             return { value: `https://anidb.net/anime/${t}` };
           case "AniList":
             return { value: `https://anilist.co/anime/${t}` };
-          case "Letterboxd":
-            return { value: `https://letterboxd.com/film/${t}` };
-          case "TVmaze":
-            return { value: `https://tvmaze.com/shows/${t}` };
         }
       });
   }
-  links(e, t, i) {
-    if (!e) throw new Error("An ID is required");
-    const o = this._property(t),
-      r = this._item(i),
-      n = `\n      SELECT DISTINCT ?item ?itemLabel ?IMDb ?TMDb_movie ?TMDb_tv ?TVDb_movie ?TVDb_tv ?Trakt ?RottenTomatoes ?Metacritic ?MyAnimeList ?AniDB ?AniList ?Letterboxd ?TVmaze WHERE {\n        ?item p:${o} ?statement0.\n        ?statement0 ps:${o} "${e}".\n        ?item p:P31 ?statement1.\n        ?statement1 (ps:P31/(wdt:P279*)) wd:${r}.\n        MINUS {\n          ?item p:P31 ?statement2.\n          ?statement2 (ps:P31/(wdt:P279*)) wd:Q3464665.\n        }\n        MINUS {\n          ?item p:P31 ?statement3.\n          ?statement3 (ps:P31/(wdt:P279*)) wd:Q21191270.\n        }\n        OPTIONAL { ?item wdt:P345 ?IMDb. }\n        OPTIONAL { ?item wdt:P4947 ?TMDb_movie. }\n        OPTIONAL { ?item wdt:P4983 ?TMDb_tv. }\n        OPTIONAL { ?item wdt:P12196 ?TVDb_movie. }\n        OPTIONAL { ?item wdt:P4835 ?TVDb_tv. }\n        OPTIONAL { ?item wdt:P8013 ?Trakt. }\n        OPTIONAL { ?item wdt:P1258 ?RottenTomatoes. }\n        OPTIONAL { ?item wdt:P1712 ?Metacritic. }\n        OPTIONAL { ?item wdt:P4086 ?MyAnimeList. }\n        OPTIONAL { ?item wdt:P5646 ?AniDB. }\n        OPTIONAL { ?item wdt:P8729 ?AniList. }\n        OPTIONAL { ?item wdt:P6127 ?Letterboxd. }\n        OPTIONAL { ?item wdt:P8600 ?TVmaze. }\n        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }\n      }\n      LIMIT 1000\n      `;
-    return new Promise((i, o) => {
+
+  links(id, idSource, itemType) {
+    if (!id) throw new Error("An ID is required");
+    const property = this._property(idSource);
+    const item = this._item(itemType);
+    const query = `
+      SELECT DISTINCT ?item ?itemLabel ?IMDb ?TMDb_movie ?TMDb_tv ?TVDb_movie ?TVDb_tv ?Trakt ?RottenTomatoes ?Metacritic ?MyAnimeList ?AniDB ?AniList ?Letterboxd ?TVmaze WHERE {
+        ?item p:${property} ?statement0.
+        ?statement0 ps:${property} "${id}".
+        ?item p:P31 ?statement1.
+        ?statement1 (ps:P31/(wdt:P279*)) wd:${item}.
+        MINUS {
+          ?item p:P31 ?statement2.
+          ?statement2 (ps:P31/(wdt:P279*)) wd:Q3464665.
+        }
+        MINUS {
+          ?item p:P31 ?statement3.
+          ?statement3 (ps:P31/(wdt:P279*)) wd:Q21191270.
+        }
+        OPTIONAL { ?item wdt:P345 ?IMDb. }
+        OPTIONAL { ?item wdt:P4947 ?TMDb_movie. }
+        OPTIONAL { ?item wdt:P4983 ?TMDb_tv. }
+        OPTIONAL { ?item wdt:P12196 ?TVDb_movie. }
+        OPTIONAL { ?item wdt:P4835 ?TVDb_tv. }
+        OPTIONAL { ?item wdt:P8013 ?Trakt. }
+        OPTIONAL { ?item wdt:P1258 ?RottenTomatoes. }
+        OPTIONAL { ?item wdt:P1712 ?Metacritic. }
+        OPTIONAL { ?item wdt:P6127 ?Letterboxd. }
+        OPTIONAL { ?item wdt:P8600 ?TVmaze. }
+        OPTIONAL { ?item wdt:P4086 ?MyAnimeList. }
+        OPTIONAL { ?item wdt:P5646 ?AniDB. }
+        OPTIONAL { ?item wdt:P8729 ?AniList. }
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+      }
+      LIMIT 1000
+    `;
+
+    return new Promise((resolve, reject) => {
       GM.xmlHttpRequest({
         method: "GET",
-        url: `${this._config.endpoint}/sparql?query=${encodeURIComponent(n)}`,
+        url: `${this._config.endpoint}/sparql?query=${encodeURIComponent(query)}`,
         headers: this._headers,
         timeout: 15e3,
         onload: (r) => {
           this._debug(r);
-          const n = JSON.parse(r.responseText)
-            .results.bindings.map((e) => e)
-            .find((i) => i[t].value === e);
-          n && Object.keys(n).length > 0 && Object.getPrototypeOf(n) === Object.prototype
-            ? i({
-                title: n.itemLabel ? n.itemLabel.value : void 0,
-                links: {
-                  IMDB: n.IMDb ? this._link("IMDb", n.IMDb.value) : void 0,
-                  TMDB: n.TMDb_movie || n.TMDb_tv ? (n.TMDb_movie ? this._link("TMDb_movie", n.TMDb_movie.value) : this._link("TMDb_tv", n.TMDb_tv.value)) : void 0,
-                  TVDB: n.TVDb_movie || n.TVDb_tv ? (n.TVDb_movie ? this._link("TVDb_movie", n.TVDb_movie.value) : this._link("TVDb_tv", n.TVDb_tv.value)) : void 0,
-                  Trakt: n.Trakt ? this._link("Trakt", n.Trakt.value) : void 0,
-                  "Rotten Tomatoes": n.RottenTomatoes ? this._link("Rotten Tomatoes", n.RottenTomatoes.value) : void 0,
-                  Metacritic: n.Metacritic ? this._link("Metacritic", n.Metacritic.value) : void 0,
-                  MyAnimeList: n.MyAnimeList ? this._link("MyAnimeList", n.MyAnimeList.value) : void 0,
-                  AniDB: n.AniDB ? this._link("AniDB", n.AniDB.value) : void 0,
-                  AniList: n.AniList ? this._link("AniList", n.AniList.value) : void 0,
-                  Letterboxd: n.Letterboxd ? this._link("Letterboxd", n.Letterboxd.value) : void 0,
-                  TVmaze: n.TVmaze ? this._link("TVmaze", n.TVmaze.value) : void 0,
-                },
-                item: n.item.value,
-              })
-            : o(new Error("No results"));
+          const results = JSON.parse(r.responseText)
+            .results.bindings
+            .find((item) => item[idSource].value === id);
+
+          if (
+            results &&
+            Object.keys(results).length > 0 &&
+            Object.getPrototypeOf(results) === Object.prototype
+          ) {
+            const data = {
+              title: results.itemLabel ? results.itemLabel.value : void 0,
+              links: {
+                IMDB: results.IMDb ? this._link("IMDb", results.IMDb.value) : void 0,
+                TMDB:
+                  results.TMDb_movie || results.TMDb_tv
+                    ? results.TMDb_movie
+                      ? this._link("TMDb_movie", results.TMDb_movie.value)
+                      : this._link("TMDb_tv", results.TMDb_tv.value)
+                    : void 0,
+                TVDB:
+                  results.TVDb_movie || results.TVDb_tv
+                    ? results.TVDb_movie
+                      ? this._link("TVDb_movie", results.TVDb_movie.value)
+                      : this._link("TVDb_tv", results.TVDb_tv.value)
+                    : void 0,
+                Trakt: results.Trakt ? this._link("Trakt", results.Trakt.value) : void 0,
+                "Rotten Tomatoes": results.RottenTomatoes ? this._link("Rotten Tomatoes", results.RottenTomatoes.value) : void 0,
+                Metacritic: results.Metacritic ? this._link("Metacritic", results.Metacritic.value) : void 0,
+                Letterboxd: results.Letterboxd ? this._link("Letterboxd", results.Letterboxd.value) : void 0,
+                TVmaze: results.TVmaze ? this._link("TVmaze", results.TVmaze.value) : void 0,
+                MyAnimeList: results.MyAnimeList ? this._link("MyAnimeList", results.MyAnimeList.value) : void 0,
+                AniDB: results.AniDB ? this._link("AniDB", results.AniDB.value) : void 0,
+                AniList: results.AniList ? this._link("AniList", results.AniList.value) : void 0,
+              },
+              item: results.item.value,
+            };
+
+            // If extra properties like AniList, AniDB, or MyAnimeList are missing,
+            // try to fetch them using an external API.
+            if (!data.links.AniList || !data.links.AniDB || !data.links.MyAnimeList) {
+              let externalEndpoint = null;
+              let externalId = null;
+              // Prefer a TMDb ID if available; otherwise fall back to TVDb.
+              if (results.TMDb_movie) {
+                externalEndpoint = "themoviedb";
+                externalId = results.TMDb_movie.value;
+              } else if (results.TMDb_tv) {
+                externalEndpoint = "themoviedb";
+                externalId = results.TMDb_tv.value;
+              } else if (results.TVDb_movie) {
+                externalEndpoint = "thetvdb";
+                externalId = results.TVDb_movie.value;
+              } else if (results.TVDb_tv) {
+                externalEndpoint = "thetvdb";
+                externalId = results.TVDb_tv.value;
+              }
+
+              if (externalEndpoint && externalId) {
+                GM.xmlHttpRequest({
+                  method: "GET",
+                  url: `https://arm.haglund.dev/api/v2/${externalEndpoint}?id=${externalId}`,
+                  timeout: 15e3,
+                  onload: (extRes) => {
+                    try {
+                      const extData = JSON.parse(extRes.responseText);
+                      if (Array.isArray(extData) && extData.length > 0) {
+                        const extInfo = extData[0];
+                        const mapping = {
+                          themoviedb: "TMDB",
+                          thetvdb: "TVDB",
+                          anilist: "AniList",
+                          anidb: "AniDB",
+                          myanimelist: "MyAnimeList",
+                        };
+
+                        Object.keys(mapping).forEach((apiKey) => {
+                          const linkKey = mapping[apiKey];
+                          if (!data.links[linkKey] && extInfo[apiKey]) {
+                            data.links[linkKey] =
+                              apiKey === "themoviedb" ? this._link(itemType === "movie" ? "TMDb_movie" : "TMDb_tv", extInfo[apiKey]) :
+                              apiKey === "thetvdb" ? this._link(itemType === "movie" ? "TVDb_movie" : "TVDb_tv", extInfo[apiKey]) :
+                              this._link(linkKey, extInfo[apiKey]);
+                          }
+                        });
+                      }
+                    } catch (error) {
+                      console.error("Error parsing external API response:", error);
+                    }
+                    resolve(data);
+                  },
+                  onerror: () => resolve(data),
+                  ontimeout: () => resolve(data)
+                });
+              } else {
+                resolve(data);
+              }
+            } else {
+              resolve(data);
+            }
+          } else {
+            reject(new Error("No results"));
+          }
         },
         onerror: () => {
-          o(new Error("An error occurs while processing the request"));
+          reject(new Error("An error occurs while processing the request"));
         },
         ontimeout: () => {
-          o(new Error("Request times out"));
+          reject(new Error("Request times out"));
         },
       });
     });
--- userscripts/external-links-on-trakt.user.js
+++ userscripts/external-links-on-trakt.user.js
@@ -24,6 +24,8 @@
 /* global $, NodeCreationObserver, Wikidata */
 
 (() => {
+  'use strict';
+
   const CONSTANTS = {
     CACHE_DURATION: 36E5, // 1 hour in milliseconds
     SCRIPT_ID: GM.info.script.name.toLowerCase().replace(/\s/g, '-'),
@@ -32,130 +34,10 @@
     SCRIPT_NAME: GM.info.script.name
   };
 
-  // Link providers configuration
-  const LINK_PROVIDERS = {
-    BROCOFLIX: {
-      id: 'brocoflix',
-      name: 'BrocoFlix',
-      description:
-        'Provides a direct link to the BrocoFlix streaming page for the selected title.',
-      getUrl: ({ tmdbId, type }) =>
-        `https://brocoflix.com/pages/info?id=${tmdbId}&type=${type === 'tv' ? 'tv' : 'movie'}`,
-      requires: ['tmdbId'],
-    },
-    CINEBY: {
-      id: 'cineby',
-      name: 'Cineby',
-      description:
-        'Provides a direct link to the Cineby streaming page for the selected title.',
-      getUrl: ({ tmdbId, type, season, episode }) => {
-        const baseUrl = 'https://www.cineby.app';
-        const mediaType = type === 'tv' ? 'tv' : 'movie';
-        return `${baseUrl}/${mediaType}/${tmdbId}${
-          type === 'tv' ? `/${season}/${episode}` : ''
-        }`;
-      },
-      requires: ['tmdbId'],
-    },
-    DMM: {
-      id: 'dmm',
-      name: 'DMM',
-      description:
-        'Provides a direct link to Debrid Media Manager for the selected title.',
-      getUrl: ({ imdbId, type, season }) => {
-        const baseUrl = 'https://debridmediamanager.com';
-        const mediaType = type === 'tv' ? 'show' : 'movie';
-        return `${baseUrl}/${mediaType}/${imdbId}${type === 'tv' ? `/${season}` : ''}`;
-      },
-      requires: ['imdbId'],
-    },
-    FREEK: {
-      id: 'freek',
-      name: 'Freek',
-      description:
-        'Provides a direct link to the Freek streaming page for the selected title.',
-      getUrl: ({ tmdbId, type, season, episode }) => {
-        const baseUrl = 'https://freek.to';
-        const mediaType = type === 'tv' ? 'tv' : 'movie';
-        return `${baseUrl}/watch/${mediaType}/${tmdbId}${
-          type === 'tv' ? `?season=${season}&ep=${episode}` : ''
-        }`;
-      },
-      requires: ['tmdbId'],
-    },
-    MEDIUX: {
-      id: 'mediux',
-      name: 'Mediux',
-      description:
-        'Provides a direct link to the Mediux Poster site for the selected title.',
-      getUrl: ({ tmdbId, type }) =>
-        `https://mediux.pro/${type === 'tv' ? 'shows' : 'movies'}/${tmdbId}`,
-      requires: ['tmdbId'],
-    },
-    PSTREAM: {
-      id: 'pstream',
-      name: 'P-Stream',
-      description:
-        'Provides a direct link to the P-Stream embedded player for the selected title.',
-      getUrl: ({ tmdbId, type, season, episode }) => {
-        const baseUrl = 'https://iframe.pstream.org';
-        const mediaType = type === 'tv' ? 'tv' : 'movie';
-        return `${baseUrl}/embed/tmdb-${mediaType}-${tmdbId}${
-          type === 'tv' ? `/${season}/${episode}` : ''
-        }`;
-      },
-      requires: ['tmdbId'],
-    },
-    RIVE: {
-      id: 'rive',
-      name: 'Rive',
-      description:
-        'Provides a direct link to the Rive streaming page for the selected title.',
-      getUrl: ({ tmdbId, type, season, episode }) => {
-        const baseUrl = 'https://rivestream.live';
-        const mediaType = type === 'tv' ? 'tv' : 'movie';
-        return `${baseUrl}/watch?type=${mediaType}&id=${tmdbId}${
-          type === 'tv' ? `&season=${season}&episode=${episode}` : ''
-        }`;
-      },
-      requires: ['tmdbId'],
-    },
-    WOVIE: {
-      id: 'wovie',
-      name: 'Wovie',
-      description:
-        'Provides a direct link to the Wovie streaming page for the selected title.',
-      getUrl: ({ tmdbId, type, sanitizedTitle, season, episode }) => {
-        const baseUrl = 'https://wovie.vercel.app';
-        const mediaType = type === 'tv' ? 'tv' : 'movie';
-        return `${baseUrl}/play/${mediaType}/${tmdbId}/${sanitizedTitle}${
-          type === 'tv' ? `?season=${season}&episode=${episode}` : ''
-        }`;
-      },
-      requires: ['tmdbId'],
-    },
-    XPRIME: {
-      id: 'xprime',
-      name: 'XPrime',
-      description:
-        'Provides a direct link to the XPrime streaming page for the selected title.',
-      getUrl: ({ tmdbId, type, season, episode }) => {
-        const baseUrl = 'https://xprime.tv';
-        return `${baseUrl}/watch/${tmdbId}${
-          type === 'tv' ? `/${season}/${episode}` : ''
-        }`;
-      },
-      requires: ['tmdbId'],
-    },
-  };
-
   // Default configuration
   const DEFAULT_CONFIG = {
     logging: false,
-    debugging: false,
-    enabledLinks: Object.fromEntries(
-      Object.values(LINK_PROVIDERS).map(({ id }) => [id, false])
-    )
+    debugging: false
   };
 
   class TraktExternalLinks {
@@ -237,8 +119,6 @@
         if (this.mediaInfo.imdbId) {
           await this.processWikidataLinks();
         }
-
-        this.addDirectLinks();
       } catch (error) {
         this.error(`Failed handling external links: ${error.message}`);
       }
@@ -247,74 +127,13 @@
     getMediaInfo() {
       const pathParts = location.pathname.split('/');
       const type = pathParts[1] === 'movies' ? 'movie' : 'tv';
-      const sanitizedTitle = pathParts[2] === 'seasons' || pathParts[2] === 'episodes'
-        ? pathParts[2]
-        : this.sanitizeTitle(pathParts[2]);
-
       const imdbLink = $('#external-link-imdb');
-      const tmdbLink = $('#external-link-tmdb');
-
-      const tmdbDetails = tmdbLink.length ? this.extractDetailsFromUrl(tmdbLink.attr('href')) : null;
       const imdbId = imdbLink.length ? imdbLink.attr('href')?.match(/tt\d+/)?.[0] : null;
 
       return {
         type,
-        sanitizedTitle,
-        imdbId,
-        tmdbId: tmdbDetails?.tmdbId,
-        season: tmdbDetails?.season,
-        episode: tmdbDetails?.episode
+        imdbId
       };
-    }
-
-    sanitizeTitle(title) {
-      try {
-        return title.replace(/-\d{4}$/, '');
-      } catch (error) {
-        this.error(`Failed to sanitize title: ${error.message}`);
-        return title;
-      }
-    }
-
-    extractDetailsFromUrl(url) {
-      try {
-        const parts = url.split('/');
-        return {
-          tmdbId: parts[4],
-          season: parts[6] || '1',
-          episode: parts[8] || '1'
-        };
-      } catch (error) {
-        this.error(`Failed to extract URL details: ${error.message}`);
-        return {
-          tmdbId: null,
-          season: '1',
-          episode: '1'
-        };
-      }
-    }
-
-    addDirectLinks() {
-      Object.values(LINK_PROVIDERS).forEach(provider => {
-        try {
-          if (this.shouldAddProvider(provider)) {
-            this.createDirectLink(provider);
-          }
-        } catch (error) {
-          this.error(`Error creating ${provider.name} link: ${error.message}`);
-        }
-      });
-    }
-
-    shouldAddProvider(provider) {
-      const isConfigurable = provider.id && this.config.enabledLinks[provider.id];
-      const isNonConfigurable = !provider.id;
-      return (isConfigurable || isNonConfigurable) &&
-             this.areRequirementsMet(provider.requires);
-    }
-
-    areRequirementsMet(requiredFields) {
-      return requiredFields.every(field => this.mediaInfo[field]);
     }
 
     createLink(name, url) {
@@ -325,11 +144,6 @@
         $('#info-wrapper .sidebar .external li a:not(:has(i))').last().after(linkHtml);
         this.debug(`Added ${name} link: ${url}`);
       }
-    }
-
-    createDirectLink(provider) {
-      const url = provider.getUrl(this.mediaInfo);
-      this.createLink(provider.name, url);
     }
 
     async processWikidataLinks() {
@@ -430,11 +244,6 @@
                   </label>
                 </div>
               </div>
-
-              <div class="settings-section">
-                <h3><i class="fas fa-external-link-alt"></i> Extra External Links</h3>
-                ${this.generateExternalLinksSettings()}
-              </div>
             </div>
 
             <div class="modal-footer">
@@ -446,26 +255,9 @@
       `;
     }
 
-    generateExternalLinksSettings() {
-      return Object.values(LINK_PROVIDERS)
-        .filter(provider => provider.id)
-        .map(provider => `
-          <div class="setting-item">
-            <div class="setting-info">
-              <label for="${provider.id}">${provider.name}</label>
-              <div class="description">${provider.description}</div>
-            </div>
-            <label class="switch">
-              <input type="checkbox" id="${provider.id}"
-                ${this.config.enabledLinks[provider.id] ? 'checked' : ''}>
-              <span class="slider"></span>
-            </label>
-          </div>
-        `).join('');
-    }
-
     addModalStyles() {
-      const styles = `#${CONSTANTS.SCRIPT_ID}-config{position:fixed;top:0;left:0;width:100%;height:100%;background:rgba(0,0,0,0.7);z-index:9999;display:flex;justify-content:center;align-items:center}.modal-content{background:#2b2b2b;color:#fff;border-radius:8px;width:450px;max-width:90%;box-shadow:0 8px 32px rgba(0,0,0,0.3)}.modal-header{padding:1.5rem;border-bottom:1px solid #404040;position:relative;display:flex;justify-content:space-between;align-items:center}.modal-header h2{margin:0;font-size:1.4rem;color:#fff}.close-button{background:none;border:none;color:#fff;font-size:1.5rem;cursor:pointer;padding:0 0.5rem}.settings-sections{padding:1.5rem;max-height:60vh;overflow-y:auto}.settings-section{margin-bottom:2rem}.settings-section h3{font-size:1.1rem;margin:0 0 1.2rem;color:#fff;display:flex;align-items:center;gap:0.5rem}.setting-item{display:flex;justify-content:space-between;align-items:center;padding:0.8rem 0;border-bottom:1px solid #404040}.setting-info{flex-grow:1;margin-right:1.5rem}.setting-info label{display:block;font-weight:500;margin-bottom:0.3rem}.description{color:#a0a0a0;font-size:0.9rem;line-height:1.4}.switch{flex-shrink:0}.modal-footer{padding:1.5rem;border-top:1px solid #404040;display:flex;gap:0.8rem;justify-content:flex-end}.btn{padding:0.6rem 1.2rem;border-radius:4px;border:none;cursor:pointer;font-weight:500;transition:all 0.2s ease}.btn.save{background:#4CAF50;color:#fff}.btn.warning{background:#f44336;color:#fff}.btn:hover{opacity:0.9}`;
+      // eslint-disable-next-line max-len
+      const styles = `#${CONSTANTS.SCRIPT_ID}-config{position:fixed;top:0;left:0;width:100%;height:100%;background:rgba(0,0,0,0.7);z-index:9999;display:flex;justify-content:center;align-items:center}#${CONSTANTS.SCRIPT_ID}-config .modal-content{background:#2b2b2b;color:#fff;border-radius:8px;width:450px;max-width:90%;box-shadow:0 8px 32px rgba(0,0,0,0.3)}#${CONSTANTS.SCRIPT_ID}-config .modal-header{padding:1.5rem;border-bottom:1px solid #404040;position:relative;display:flex;justify-content:space-between;align-items:center}#${CONSTANTS.SCRIPT_ID}-config .modal-header h2{margin:0;font-size:1.4rem;color:#fff}#${CONSTANTS.SCRIPT_ID}-config .close-button{background:none;border:none;color:#fff;font-size:1.5rem;cursor:pointer;padding:0 0.5rem}#${CONSTANTS.SCRIPT_ID}-config .settings-sections{padding:1.5rem;max-height:60vh;overflow-y:auto}#${CONSTANTS.SCRIPT_ID}-config .settings-section{margin-bottom:2rem}#${CONSTANTS.SCRIPT_ID}-config .settings-section h3{font-size:1.1rem;margin:0 0 1.2rem;color:#fff;display:flex;align-items:center;gap:0.5rem}#${CONSTANTS.SCRIPT_ID}-config .setting-item{display:flex;justify-content:space-between;align-items:center;padding:0.8rem 0;border-bottom:1px solid #404040}#${CONSTANTS.SCRIPT_ID}-config .setting-info{flex-grow:1;margin-right:1.5rem}#${CONSTANTS.SCRIPT_ID}-config .setting-info label{display:block;font-weight:500;margin-bottom:0.3rem}#${CONSTANTS.SCRIPT_ID}-config .description{color:#a0a0a0;font-size:0.9rem;line-height:1.4}#${CONSTANTS.SCRIPT_ID}-config .switch{flex-shrink:0}#${CONSTANTS.SCRIPT_ID}-config .modal-footer{padding:1.5rem;border-top:1px solid #404040;display:flex;gap:0.8rem;justify-content:flex-end}#${CONSTANTS.SCRIPT_ID}-config .btn{padding:0.6rem 1.2rem;border-radius:4px;border:none;cursor:pointer;font-weight:500;transition:all 0.2s ease}#${CONSTANTS.SCRIPT_ID}-config .btn.save{background:#4CAF50;color:#fff}#${CONSTANTS.SCRIPT_ID}-config .btn.warning{background:#f44336;color:#fff}#${CONSTANTS.SCRIPT_ID}-config .btn:hover{opacity:0.9}`;
       $('<style>').prop('type', 'text/css').html(styles).appendTo('head');
     }
 
@@ -475,12 +267,6 @@
       $('#save-config').click(async () => {
         this.config.logging = $('#logging').is(':checked');
         this.config.debugging = $('#debugging').is(':checked');
-
-        Object.values(LINK_PROVIDERS)
-          .filter(provider => provider.id)
-          .forEach(provider => {
-            this.config.enabledLinks[provider.id] = $(`#${provider.id}`).is(':checked');
-          });
 
         await GM.setValue(CONSTANTS.CONFIG_KEY, this.config);
         $(`#${CONSTANTS.SCRIPT_ID}-config`).remove();
